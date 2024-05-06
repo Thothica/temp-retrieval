2f155c8af068aa1568f50382034877adef48440d
Tejas013 <tejasmadhukar6@gmail.com>
2024-05-07 02:23:24 +0530

    add done ugh

--- main.go
+++ main.go
@@ -69,7 +69,86 @@ func init() {
 
 func main() {
 	http.HandleFunc("POST /arabic-poems", HandleArabicPoems)
+    http.HandleFunc("POST /cleaned-dutchtext", HandleCleanedDutchText)
 	log.Fatal(http.ListenAndServe(":80", nil))
+}
+
+func HandleCleanedDutchText(w http.ResponseWriter, r *http.Request) {
+	var dutchtextrequest RequestBody
+
+	defer r.Body.Close()
+
+	err := json.NewDecoder(r.Body).Decode(&dutchtextrequest)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	searchBody := strings.NewReader(fmt.Sprintf(`{
+                "_source": {
+                        "excludes": [
+                                "Raw_Response_embedding"
+                        ]
+                },
+                "query": {
+                        "neural": {
+                                "Raw_Response_embedding": {
+                                        "query_text": "%v",
+                                        "model_id": "AbDZGo8BB3UUeZ_94CHA",
+                                        "k": %v
+                                }
+                        }
+                },
+                "size": %v
+        }`, dutchtextrequest.Query, dutchtextrequest.K, dutchtextrequest.Size))
+
+	semanticSearchRequest := opensearchapi.SearchRequest{
+		Index: []string{"cleaned-dutchtext-index"},
+		Body:  searchBody,
+	}
+
+	var searchResponse map[string]interface{}
+
+	res, err := semanticSearchRequest.Do(context.Background(), c)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+	defer res.Body.Close()
+
+	err = json.NewDecoder(res.Body).Decode(&searchResponse)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+
+	data := searchResponse["hits"].(map[string]interface{})["hits"].([]interface{})
+
+	for idx, val := range data {
+		valMap, ok := val.(map[string]interface{})
+		if !ok {
+			continue
+		}
+
+		source := valMap["_source"].(map[string]interface{})
+
+		source["Results"] = fmt.Sprintf(`Title: %v
+
+Translated Text:
+%v
+
+Interpretation:
+%v`, source["title"], source["translation"], source["interpretation"])
+
+		valMap["_source"] = source
+		data[idx] = valMap
+	}
+
+	responseData, err := json.Marshal(data)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+
+	w.WriteHeader(http.StatusOK)
+	w.Write(responseData)
 }
 
 func HandleArabicPoems(w http.ResponseWriter, r *http.Request) {
