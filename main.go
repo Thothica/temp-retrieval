62f671ff355051e091ce7c0554c00f62a2de526b
Tejas013 <tejasmadhukar6@gmail.com>
2024-07-06 06:50:53 +0530

    New Index

--- main.go
+++ main.go
@@ -76,6 +76,7 @@ func main() {
 	http.Handle("POST /legaltext", httplog.Logger(http.HandlerFunc(HandleLegalText)))
 	http.Handle("POST /loc", httplog.Logger(http.HandlerFunc(HandleLoc)))
 	http.Handle("POST /indian-lit", httplog.Logger(http.HandlerFunc(HandleIndianLit)))
+	http.Handle("POST /openalex", httplog.Logger(http.HandlerFunc(HandleOpenalex)))
 	http.Handle("/not_found", httplog.Logger(http.NotFoundHandler()))
 	log.Fatal(http.ListenAndServe(":3000", nil))
 }
@@ -135,6 +136,48 @@ func SemanitcSearch(body *strings.Reader, index string, sourceTransformation fun
 	}
 
 	return responseData, nil
+}
+
+func HandleOpenalex(w http.ResponseWriter, r *http.Request) {
+	var req RequestBody
+
+	defer r.Body.Close()
+
+	err := json.NewDecoder(r.Body).Decode(&req)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	searchBody := strings.NewReader(fmt.Sprintf(`{
+                "_source": {
+                        "excludes": [
+                                "Text_embedding"
+                        ]
+                },
+                "query": {
+                        "neural": {
+                                "Text_embedding": {
+                                        "query_text": "%v",
+                                        "model_id": "AbDZGo8BB3UUeZ_94CHA",
+                                        "k": %v
+                                }
+                        }
+                },
+                "size": %v
+        }`, req.Query, req.K, req.Size))
+
+	sourceTransformation := func(sourceOrg *map[string]interface{}) {
+	}
+
+	res, err := SemanitcSearch(searchBody, "openalex-index", sourceTransformation)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	w.Write(res)
 }
 
 func HandleIndianLit(w http.ResponseWriter, r *http.Request) {
